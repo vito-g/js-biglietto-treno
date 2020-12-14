326ed6cb06a10a2189b4c6b11752136e5bf9b3b1
Vito Gentile <gentilevito1076@gmail.com>
2020-12-14 15:05:09 +0100

    Start

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var percorrenza = prompt('Salve! Inserisca la percorrenza sino a destinazione');
+console.log('La sua percorrenza è di: ' + percorrenza + 'km.');
+var età = prompt('Adesso, inserisca la sua età');
+console.log('La sua età è di: ' + età + 'anni');
