c6dcd77db1e09e21de6757b50d2cae0daa77517a
Vito Gentile <gentilevito1076@gmail.com>
2020-12-14 15:12:22 +0100

    Calcolo del Default Cost

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 var percorrenza = prompt('Salve! Inserisca la percorrenza sino a destinazione');
 console.log('La sua percorrenza è di: ' + percorrenza + 'km.');
+
 var età = prompt('Adesso, inserisca la sua età');
 console.log('La sua età è di: ' + età + 'anni');
+
+var defaultCost = 0.21 * percorrenza;
+console.log('Il costo predefinito del biglietto è di: ' + defaultCost + 'euro')
