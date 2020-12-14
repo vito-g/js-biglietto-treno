d4dbf7581fe8ce14b9a23f4b30cdc72957c24aa8
Vito Gentile <gentilevito1076@gmail.com>
2020-12-14 17:00:58 +0100

    BONUS: Condizione a monte di inserimento solo dati numerico

---
+++ js/script-bonus.js
@@ -0,0 +1,26 @@
+var percorrenza = prompt('Salve! Inserisca la percorrenza sino a destinazione');
+console.log('La sua percorrenza è di: ' + percorrenza + 'km.');
+
+var età = prompt('Adesso, inserisca la sua età');
+console.log('La sua età è di: ' + età + 'anni');
+
+var defaultCost = 0.21 * percorrenza;
+console.log('Il costo predefinito del biglietto è di: ' + defaultCost + 'euro');
+
+var discountedCost = document.getElementById('output');
+
+
+if (isNaN(percorrenza) || (isNaN(età))) {
+  alert('Inserisca solo valori numerici. Ripeta l\'operazione.');
+} else {
+if ( età < 18) {
+  discountedCost.innerHTML = 'Il costo del biglietto per la sua fascia d\'età è di: ' + (defaultCost * 0.8) + 'euro';
+  console.log('Il costo del biglietto per la sua fascia d\'età è di: ' + (defaultCost * 0.8) + 'euro');
+} else if ( età > 65) {
+  discountedCost.innerHTML = 'Il costo del biglietto per la sua fascia d\'età è di: ' + (defaultCost * 0.6) + 'euro';
+  console.log('Il costo del biglietto per la sua fascia d\'età è di: ' + (defaultCost * 0.6) + 'euro');
+} else {
+  discountedCost.innerHTML = 'Il costo del biglietto per la sua fascia d\'età è di: ' + defaultCost + 'euro';
+  console.log('Il costo del biglietto per la sua fascia d\'età è di: ' + defaultCost + 'euro');
+}
+}
