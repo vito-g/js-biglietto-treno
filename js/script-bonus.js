0078d0b9ba8b95a001664394181147bba0b742d4
Vito Gentile <gentilevito1076@gmail.com>
2020-12-14 18:28:06 +0100

    Inserimento output aggiuntivi nel DOM

--- js/script-bonus.js
+++ js/script-bonus.js
@@ -1,8 +1,12 @@
 var percorrenza = prompt('Salve! Inserisca la percorrenza sino a destinazione');
 console.log('La sua percorrenza è di: ' + percorrenza + 'km.');
+var firstData = document.getElementById('data1');
+firstData.innerHTML = 'La sua percorrenza è di: ' + percorrenza + 'km.';
 
 var età = prompt('Adesso, inserisca la sua età');
 console.log('La sua età è di: ' + età + 'anni');
+var secondData = document.getElementById('data2');
+secondData.innerHTML = 'La sua età è di: ' + età + 'anni';
 
 var defaultCost = 0.21 * percorrenza;
 console.log('Il costo predefinito del biglietto è di: ' + defaultCost + 'euro');
